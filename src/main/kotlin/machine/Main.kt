489768d550f94fe6958669cae9f6bf105e916893
coriolang <yan.radi@gmail.com>
2021-10-15 23:44:10 +0300

    Estimate the number of servings

--- src/main/kotlin/machine/Main.kt
+++ src/main/kotlin/machine/Main.kt
@@ -1,15 +1,31 @@
 package machine
 
 fun main(args: Array<String>) {
+    val waterPerCup = 200
+    val milkPerCup = 50
+    val beansPerCup = 15
+
+    println("Write how many ml of water the coffee machine has: > ")
+    val currentWater: Int = readLine()?.toIntOrNull() ?: 0
+    println("Write how many ml of milk the coffee machine has: > ")
+    val currentMilk: Int = readLine()?.toIntOrNull() ?: 0
+    println("Write how many grams of coffee beans the coffee machine has: > ")
+    val currentBeans: Int = readLine()?.toIntOrNull() ?: 0
     println("Write how many cups of coffee you will need: > ")
-    val cupsNumber: Int = readLine()?.toIntOrNull() ?: 0
+    val neededCups: Int = readLine()?.toIntOrNull() ?: 0
 
-    val water = cupsNumber * 200
-    val milk = cupsNumber * 50
-    val beans = cupsNumber * 15
+    var possibleCups: Int = currentWater / waterPerCup
+    if (possibleCups > currentMilk / milkPerCup) {
+        possibleCups = currentMilk / milkPerCup
+    } else if (possibleCups > currentBeans / beansPerCup) {
+        possibleCups = currentBeans / beansPerCup
+    }
 
-    println("For $cupsNumber cups of coffee you will need:")
-    println("$water ml of water")
-    println("$milk ml of milk")
-    println("$beans g of coffee beans")
+    if (possibleCups < neededCups) {
+        println("No, I can make only $possibleCups cups of coffee")
+    } else if (possibleCups == neededCups) {
+        println("Yes, I can make that amount of coffee")
+    } else {
+        println("Yes, I can make that amount of coffee (and even ${possibleCups - neededCups} more than that)")
+    }
 }
